43afcda78dd423554ed61c81679af8df8b911317
Bob Pakhriev <boburpakhriev@gmail.com>
2022-07-10 08:16:27 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,130 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+const passwordOne = document.getElementById("password--value__one");
+const passwordTwo = document.getElementById("password--value__two");
+
+const userLength = document.getElementById("length");
+
+function copyOnClick(password) {
+  password.addEventListener("copy", (e) => {
+    e.preventDefault();
+    if (e.clipboardData) {
+      e.clipboardData.setData("text/plain", password.textContent);
+      console.log(e.clipboardData.getData("text"));
+    }
+  });
+  document.execCommand("copy");
+  showSnackBar();
+}
+
+function generatePassword() {
+  let firstPassword = "";
+  let secondPassword = "";
+  for (let i = 0; i < userLength.value; i++) {
+    firstPassword += characters[Math.floor(Math.random() * characters.length)];
+    secondPassword += characters[Math.floor(Math.random() * characters.length)];
+  }
+
+  passwordOne.textContent = firstPassword;
+  passwordTwo.textContent = secondPassword;
+}
+
+function showSnackBar() {
+  const sb = document.getElementById("snackbar");
+  sb.className = "show";
+  setTimeout(() => {
+    sb.className = sb.className.replace("show", "");
+  }, 3000);
+}
